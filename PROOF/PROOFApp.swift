54d5acd68c74f3cca1c5b0ff169fecb9c833f548
Adrian Mazek <a6r14n@gmail.com>
2026-03-15 17:45:15 +0100

    init

---
+++ PROOF/PROOFApp.swift
@@ -0,0 +1,17 @@
+//
+//  PROOFApp.swift
+//  PROOF
+//
+//  Created by Adrian Mazek on 15/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct PROOFApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
